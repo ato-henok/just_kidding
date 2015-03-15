9d212a3c865cb6f9e6cbc17f23ee61985c4618ab
Henok Weldemicael <Ato_Henok@Henoks-Mac.local>
2015-03-15 19:00:20 -0400

    Initial Commit. Layout of basic table created. Compose page added.

---
+++ JustKidding/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  JustKidding
+//
+//  Created by Henok Weldemicael on 3/15/15.
+//  Copyright (c) 2015 Henok WeldeMicael. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
