a2134e1e02becfd517990b6133d56e7d3a396a9c
Henok Weldemicael <Ato_Henok@Henoks-Mac.local>
2015-03-22 05:23:31 -0400

    User Profile page created.

--- JustKidding/UserProfileCell.swift
+++ JustKidding/UserProfileCell.swift
@@ -1,24 +1,24 @@
 //
-//  EntryTableViewCell.swift
+//  UserProfileCell.swift
 //  JustKidding
 //
-//  Created by Henok Weldemicael on 3/15/15.
+//  Created by Henok Weldemicael on 3/22/15.
 //  Copyright (c) 2015 Henok WeldeMicael. All rights reserved.
 //
 
 import UIKit
 
-class EntryTableViewCell: UITableViewCell {
-
+class UserProfileCell: UITableViewCell {
+    
     @IBOutlet var dateLabel: UILabel! = UILabel()
-    @IBOutlet var usernameLabel:  UIButton! = UIButton()
+    @IBOutlet var usernameLabel: UILabel! = UILabel()
     @IBOutlet var likesCountLabel: UILabel! = UILabel()
     @IBOutlet var jokeLabel: UILabel! = UILabel()
     
     @IBOutlet var roseBtn: UIButton! = UIButton()
     @IBOutlet var tomatoBtn: UIButton! = UIButton()
     @IBOutlet var favBtn: UIButton! = UIButton()
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- JustKidding/UserProfileViewController.swift
+++ JustKidding/UserProfileViewController.swift
@@ -2,19 +2,21 @@
 //  TimelineTableViewController.swift
 //  JustKidding
 //
-//  Created by Henok Weldemicael on 3/15/15.
+//  Created by Henok Weldemicael on 3/22/15.
 //  Copyright (c) 2015 Henok WeldeMicael. All rights reserved.
 //
 
 import UIKit
 import Parse
 
-class TimelineTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-   /*
+class UserProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    /*
     override func prefersStatusBarHidden() -> Bool {
-        return true
+    return true
     }
     */
+    
+    
     
     var timelineData:NSMutableArray = NSMutableArray()
     var favArray:NSMutableArray = NSMutableArray()
@@ -23,6 +25,7 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
     var jokeObj = PFObject(className: "Jokes")
     var senderName = NSString()
     
+    @IBOutlet var senderNameLabel: UILabel! = UILabel()
     @IBOutlet var tableView: UITableView!
     
     // Load
@@ -32,18 +35,19 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
         self.timelineData.removeAllObjects()
         
         var query = PFQuery(className: "Jokes")
-        query.whereKey("isOnStage", equalTo: true)
-        query.orderByDescending("updatedAt")
+        query.whereKey("senderName", equalTo: self.senderName)
+        query.orderByDescending("createdAt")
         query.findObjectsInBackgroundWithBlock {
             (objects: [AnyObject]!, error: NSError!) -> Void in
             if error == nil {
                 // The find succeeded.
-                println("\(objects.count) jokes on stage.")
+                println("\(objects.count) jokes from user \(self.senderName).")
                 // Do something with the found objects
                 if let objects = objects as? [PFObject] {
                     for object in objects {
                         self.timelineData.addObject(object)
                     }
+                    
                 }
                 
                 self.tableView.reloadData()
@@ -79,7 +83,7 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
                 println("Error: \(error) \(error.userInfo!)")
             }
         }
-
+        
     }
     
     
@@ -91,157 +95,26 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
         if(PFUser.currentUser() != nil){
             self.loadFavs()
         }
+        self.senderNameLabel.text = self.senderName as NSString
+            
         
-        if(PFUser.currentUser() == nil){
-            
-            //###########################################################################
-            // Alert for Signing up or loggin in
-            
-            var alert:UIAlertController = UIAlertController(title: "Welcome", message: "You need to signup or login in order to post", preferredStyle: UIAlertControllerStyle.Alert)
-            
-
-            alert.addAction(UIAlertAction(title: "Login", style: UIAlertActionStyle.Default, handler: {
-                alertAction in
-                
-                //********************************************************************
-                
-                var loginAlert:UIAlertController = UIAlertController(title: "Login", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
-                
-                // Username textfield created with placeholder
-                loginAlert.addTextFieldWithConfigurationHandler({
-                    
-                    textfield in
-                    textfield.placeholder = "Username"
-                    
-                })
-                
-                // Password textfield created with placeholder
-                loginAlert.addTextFieldWithConfigurationHandler({
-                    
-                    textfield in
-                    textfield.placeholder = "Password"
-                    textfield.secureTextEntry = true
-                    
-                })
-                
-                // Action for Login button
-                loginAlert.addAction(UIAlertAction(title: "Login", style: UIAlertActionStyle.Default, handler: {
-                    alertAction in
-                    
-                    let textFields:NSArray = loginAlert.textFields! as NSArray
-                    
-                    let usernameTextField:UITextField = textFields.objectAtIndex(0) as UITextField
-                    let passwordTextField:UITextField = textFields.objectAtIndex(1) as UITextField
-                    
-                    PFUser.logInWithUsernameInBackground(usernameTextField.text, password: passwordTextField.text){ (user:PFUser!, error:NSError!) -> Void in
-                        
-                        if((user) != nil){
-                            println("Login success!")
-                        }else{
-                            println(error)
-                        }
-                        
-                        
-                        
-                    }
-                    
-                }))
-                self.presentViewController(loginAlert, animated: true, completion: nil)
-                //********************************************************************
-                
-                
-                
-            }))
-            
-            
-            alert.addAction(UIAlertAction(title: "Signup", style: UIAlertActionStyle.Default, handler: {
-                alertAction in
-                //********************************************************************
-                var signupAlert:UIAlertController = UIAlertController(title: "New Account", message: "Enter the following info to signup", preferredStyle: UIAlertControllerStyle.Alert)
-                
-                // Email textfield created with placeholder
-                signupAlert.addTextFieldWithConfigurationHandler({
-                    
-                    textfield in
-                    textfield.placeholder = "Email"
-                    
-                })
-                
-                // Username textfield created with placeholder
-                signupAlert.addTextFieldWithConfigurationHandler({
-                    
-                    textfield in
-                    textfield.placeholder = "Username"
-                    
-                })
-                
-                // Password textfield created with placeholder
-                signupAlert.addTextFieldWithConfigurationHandler({
-                    
-                    textfield in
-                    textfield.placeholder = "Password"
-                    textfield.secureTextEntry = true
-                    
-                })
-                
-                
-                // Action for Login button
-                signupAlert.addAction(UIAlertAction(title: "Signup", style: UIAlertActionStyle.Default, handler: {
-                    alertAction in
-                    
-                    let textFields:NSArray = signupAlert.textFields! as NSArray
-                    
-                    let emailTextField:UITextField = textFields.objectAtIndex(0) as UITextField
-                    let usernameTextField:UITextField = textFields.objectAtIndex(1) as UITextField
-                    let passwordTextField:UITextField = textFields.objectAtIndex(2) as UITextField
-                    
-                    
-                    var newUser:PFUser = PFUser()
-                    newUser.email = emailTextField.text
-                    newUser.username = usernameTextField.text
-                    newUser.password = passwordTextField.text
-                    newUser["aboutMe"] = "Say something badass about yourself"
-                    newUser.signUpInBackgroundWithBlock({ (success:Bool, error:NSError!) -> Void in
-                        if(success){
-                            println("New user created")
-                        }else{
-                            println(error)
-                        }
-                    })
-                    
-                }))
-                self.presentViewController(signupAlert, animated: true, completion: nil)
-                //********************************************************************
-                
-                
-                
-            }))
-            self.presentViewController(alert, animated: true, completion: nil)
-            
-            //###########################################################################
-            
-            
-            
-      
-        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "groupcell")
+        self.tableView.registerClass(UserProfileCell.self, forCellReuseIdentifier: "groupcell")
         
         
         tableView.delegate = self
         tableView.dataSource = self
         
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -254,34 +127,33 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
         return 1
     }
     
-
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return timelineData.count
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell:EntryTableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as EntryTableViewCell
-    
+        let cell:UserProfileCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UserProfileCell
+        
         let joke:PFObject = self.timelineData.objectAtIndex(indexPath.row) as PFObject
         
         cell.jokeLabel.text = joke.objectForKey("joke") as NSString
-        cell.usernameLabel.setTitle(joke.objectForKey("senderName") as NSString, forState: UIControlState.Normal)
+        cell.usernameLabel.text = joke.objectForKey("senderName") as NSString
         
-     
         
         var dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy"
         cell.dateLabel.text = dateFormatter.stringFromDate(joke.createdAt)
-
-       
+        
+        
         var likesArray = joke.objectForKey("likersArray") as NSArray
         var dislikesArray = joke.objectForKey("dislikersArray") as NSArray
-       
+        
         var net = likesArray.count - dislikesArray.count
         cell.likesCountLabel.text = String(net)
         
-       
+        
         
         var currentUser = PFUser.currentUser()
         
@@ -291,7 +163,7 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
         let tomato_empty = UIImage(named: "tomato_empty.png") as UIImage!
         let fav_empty = UIImage(named: "favorite_empty.png") as UIImage!
         let fav_selected = UIImage(named: "favorite_selected.png") as UIImage!
-      
+        
         // Update the Like and Dislike icons
         
         if(likesArray.containsObject(currentUser.objectId)){
@@ -313,7 +185,7 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
         }else{
             cell.favBtn.setBackgroundImage(fav_empty, forState: .Normal)
         }
-      
+        
         
         
         // Rose button clicked
@@ -328,39 +200,24 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
         cell.favBtn.tag = indexPath.row
         cell.favBtn.addTarget(self, action: "favBtnClicked:", forControlEvents: .TouchUpInside)
         
-        // Username label clicked
-        cell.usernameLabel.tag = indexPath.row
-        cell.usernameLabel.addTarget(self, action: "nameLabelClicked:", forControlEvents: .TouchUpInside)
-   
         
         
         
-    
-    return cell
+        
+        return cell
     }
     
     //########################################################
-    //functions for Username, Like, Dislke, and Favorite buttons
-    
-    func nameLabelClicked(sender: UIButton!) {
-        
-        println("Username label Clicked")
-        
-        var joke = self.timelineData.objectAtIndex(sender.tag) as PFObject
-        self.senderName = joke.objectForKey("senderName") as NSString
-        self.performSegueWithIdentifier("showUserProfile", sender: self)
-        
-        
-    }
+    //functions for Like, Dislke, and Favorite buttons
     
     func roseBtnClicked(sender: UIButton!) {
-      
+        
         println("Rose Btn Clicked")
         
         var joke = self.timelineData.objectAtIndex(sender.tag) as PFObject
         
         var likersArray = joke.objectForKey("likersArray") as NSArray
-      
+        
         var objectId:NSString = PFUser.currentUser().objectId
         if(!(likersArray.containsObject(objectId))){
             
@@ -374,7 +231,7 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
                 }
                 
             })
-      
+            
         }else if((likersArray.containsObject(objectId))){
             
             joke.removeObject(objectId, forKey: "likersArray")
@@ -392,7 +249,7 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
     }
     
     func tomatoBtnClicked(sender: UIButton!) {
-   
+        
         println("Tomato Btn Clicked")
         
         var joke = self.timelineData.objectAtIndex(sender.tag) as PFObject
@@ -452,7 +309,7 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
             PFUser.currentUser().saveInBackgroundWithBlock({ (bool:Bool, error:NSError!) -> Void in
                 if(error == nil){
                     println("_Joke favorited")
-                   
+                    
                 }else{
                     println("_Favorite error")
                 }
@@ -462,33 +319,27 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
         self.tableView.reloadData()
     }
     //########################################################
-
-
-
+    
+    
+    
     //########################################################
     //Preparation and sending of data to CommentsViewController
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         
-        if (segue.identifier? == "showComments") {
+        if segue.identifier? == "showComments" {
             
             let controller = segue.destinationViewController as CommentsViewController
             controller.commentEntry = self.jokeObj
             
-        }else if(segue.identifier? == "showUserProfile"){
-            
-            let controller = segue.destinationViewController as UserProfileViewController
-            
-            controller.senderName = self.senderName
             
         }
-        
     }
     
-
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-
+        
         self.jokeObj = self.timelineData.objectAtIndex(indexPath.row) as PFObject
         
         self.performSegueWithIdentifier("showComments", sender: self)
@@ -497,61 +348,61 @@ class TimelineTableViewController: UIViewController, UITableViewDelegate, UITabl
     
     //########################################################
     
-   
-
+    
+    
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
-        return cell
+    let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+    
+    // Configure the cell...
+    
+    return cell
     }
     */
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    // Return NO if you do not want the specified item to be editable.
+    return true
     }
     */
-
+    
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
     }
     */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
     }
     */
-
+    
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
+    // Return NO if you do not want the item to be re-orderable.
+    return true
     }
     */
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
